b1d45436aad68d8c716d9886cacca0f046e5f777
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-14 21:06:03 -0500

    Initial Commit

---
+++ MultipleDragAndDrop/MultipleDragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleDragAndDropApp.swift
+//  MultipleDragAndDrop
+//
+//  Created by Richard Witherspoon on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MultipleDragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
